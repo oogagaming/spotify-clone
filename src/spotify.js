9b343a8f916b8326b6325be83d415f0105ca3d99
Oscar-Salin <43850176+oogagaming@users.noreply.github.com>
2020-10-27 11:10:20 +0200

    first commmit
    first commit

---
+++ src/spotify.js
@@ -0,0 +1,28 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "https://spotify-clone-3691c.web.app";
+const clientId ="a77ed27d83af47639cdc065f7f2aa702"
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+    return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+  
+        return initial;
+      }, {});
+  };
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
